6b0bbd3b1cc33afc218f38d29898d673d53b3fd5
Daniel Cunha <danielcunha54@gmail.com>
2019-12-02 19:46:13 -0300

    Finished section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,7 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react component
-const App = () => <div>Hi there!</div>;
+const App = () => (
+  <div>
+    <h1>Hi there!</h1>
+  </div>
+);
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
