304bc518ef1938c44aa1d4850fe9c94d859f7f17
Daniel Cunha <danielcunha54@gmail.com>
2019-12-02 12:36:02 -0300

    Created JSX project

---
+++ jsx/src/index.js
@@ -0,0 +1,9 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => <div>Hi there!</div>;
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
