7059a5fe6530ae187edf6c995d76c374a6bfd030
Daniel Cunha <danielcunha54@gmail.com>
2019-12-04 19:09:21 -0300

    Finished pics app

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 
 import "./ImageList.css";
 
 const ImageList = ({ images }) => {
-  const htmlImages = images.map(({ id, urls, description }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const htmlImages = images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
   return <div className="image-list">{htmlImages}</div>;
