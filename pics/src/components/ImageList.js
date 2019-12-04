6a73a6173c1ac66d464a981dd2d634960dceecde
Daniel Cunha <danielcunha54@gmail.com>
2019-12-04 13:08:53 -0300

    CSS Grid system

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+import "./ImageList.css";
+
 const ImageList = ({ images }) => {
-  const imgs = images.map(({ id, urls, description }) => {
-    console.log(urls);
+  const htmlImages = images.map(({ id, urls, description }) => {
     return <img key={id} src={urls.regular} alt={description} />;
   });
 
-  return <div>{imgs}</div>;
+  return <div className="image-list">{htmlImages}</div>;
 };
 
 export default ImageList;
