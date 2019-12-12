0eee8b4509e5555eff1ad357076029d524c1d86a
Daniel Cunha <danielcunha54@gmail.com>
2019-12-12 13:08:00 -0300

    Context system

---
+++ translate/src/components/Button.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends Component {
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => (value === "english" ? `Submit` : "Enviar")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
