94772af642589b27340c71d20dd6a5f613d86938
Daniel Cunha <danielcunha54@gmail.com>
2019-12-11 22:25:17 -0300

    Partially implemented stream show

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { fetchStream } from "../../actions";
+
+class StreamShow extends Component {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchStream(id);
+  }
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = stream;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ streams }, { match }) => {
+  const id = match.params.id;
+  return { stream: streams[id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
